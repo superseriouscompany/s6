b5f5dea8f62f4698662962688ff6168d0b7ced06
Neil Sarkar <neil.sarkar@gotinder.com>
2016-09-26 00:23:45 +0100

    Use slack-rtm-test

--- test/s6.test.js
+++ test/s6.test.js
@@ -1,15 +1,14 @@
-var expect  = require('expect');
-var async   = require('async');
-var Bot     = require('tinybot');
-var slub    = require('tinybot').slub;
-var debug   = require('debug')('s6:test');
-var secrets = require('../secrets');
+var expect    = require('expect');
+var Bot       = require('tinybot');
+var slackTest = require('slack-rtm-test');
+var secrets   = require('../secrets');
+var debug     = require('debug')('s6:test');
 
 var token = secrets.token;
 
 describe('s6', function() {
   before(function(cb){
-    slub.serve(6969, function(err) {
+    slackTest.serve(6969, function(err) {
       if( err ) { return cb(err); }
       var bot = new Bot(token, 'C2EVDHF5L', 'http://localhost:6969');
       bot.start(cb);
@@ -40,20 +39,7 @@ describe('s6', function() {
       { response: 'https://image.superseriouscompany.com/janky.jpg' }
     ]
 
-    expectConversation(conversation, cb);
+    slackTest.expectConversation(conversation, cb);
   })
 
 })
-
-function expectConversation(conversation, cb) {
-  async.series(conversation.map(function(message) {
-    return function(cb) {
-      if( !!message.response ) {
-        return slub.socket.shouldReceive(message.response, cb);
-      }
-
-      slub.socket.send(message);
-      cb();
-    }
-  }), cb);
-}
